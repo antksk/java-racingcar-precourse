4be67ec1b0fa0edf3c575761f6b47bf3aa108989
newton <antksk@gmail.com>
2021-05-03 11:04:26 +0900

    fix : 레이싱카 이름 등록시 양쪽 공백 제거 처리

--- src/main/java/racingcar/domain/core/RacingCarName.java
+++ src/main/java/racingcar/domain/core/RacingCarName.java
@@ -5,20 +5,21 @@ import java.util.regex.Pattern;
 
 final class RacingCarName {
     private static final int MAX_LENGHT_NAME = 5;
+    static final String ERROR_MSG_REQUIRE_NAME = "자동차 이름은 필수로 등록해야 합니다.";
     static final String ERROR_MSG_MAX_LENGHT_NAME = "자동차 이름은 5자 이하로 등록해야 합니다.";
     static final String ERROR_MSG_VALID_NAME = "영어 소문자(a-z)로 시작하고, 특정 특수문자(-,_) 숫자를 포함할 수 있습니다";
     private static final Pattern namePattern = Pattern.compile("[a-z][a-z0-9_-]{0,4}");
     private final String name;
 
     RacingCarName(String name) {
-        requireName(name);
         validMaxLengthName(name);
         validName(name);
-        this.name = name.trim();
+        this.name = name;
     }
 
     static RacingCarName racingCarName(String name) {
-        return new RacingCarName(name);
+        requireName(name);
+        return new RacingCarName(name.trim());
     }
 
     private void validName(String name) {
@@ -33,9 +34,9 @@ final class RacingCarName {
         }
     }
 
-    private void requireName(String name) {
+    private static void requireName(String name) {
         if (null == name || "".equals(name.trim())) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MSG_REQUIRE_NAME);
         }
     }
 
--- src/test/java/racingcar/domain/core/RacingCarNameTest.java
+++ src/test/java/racingcar/domain/core/RacingCarNameTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static racingcar.domain.core.RacingCarName.*;
 
@@ -16,7 +17,8 @@ class RacingCarNameTest {
     void requiredName(String carName) {
         String testName = "null".equals(carName) ? null : carName;
         assertThatIllegalArgumentException()
-                .isThrownBy(()-> racingCarName(testName));
+                .isThrownBy(()-> racingCarName(testName))
+                .withMessage(ERROR_MSG_REQUIRE_NAME);
     }
 
     @DisplayName("레이싱카 이름은 MAX_LENGTH이하로 등록해야 함")
@@ -35,6 +37,13 @@ class RacingCarNameTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(()-> racingCarName(carName))
                 .withMessage(ERROR_MSG_VALID_NAME);
+    }
+
+    @DisplayName("정상적인 이름을 등록한 경우 확인(단, 양쪽 공백은 제거됨)")
+    @ParameterizedTest
+    @ValueSource(strings = {"a ", "  ab ", "car_1", "pobi", "a___5", "b---5"})
+    void crrectName(String carName){
+        assertThat(racingCarName(carName).toName()).isEqualTo(carName.trim());
     }
 
 }
